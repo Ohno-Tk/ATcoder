43e5a8f75e13775b659f609652e7095df5c1fd3a
ohno Takuya <ohno.takuya.1502@gmail.com>
2021-07-20 06:12:41 +0900

    ABC043_B完成

---
+++ ABCクラス/AtCoder Beginner Contest 043/B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    string input_s;
+    string output_result;
+
+    cin >> input_s;
+
+    for(int i = 0; i < input_s.length(); i++){
+
+        if(input_s[i] == '0'){
+            output_result.push_back('0');
+        }
+        else if(input_s[i] == '1'){
+            output_result.push_back('1');
+        }
+        else if(input_s[i] == 'B'){
+
+            if(!output_result.empty()){
+                output_result.pop_back();
+            }
+        }
+
+    }
+
+    cout << output_result << endl;
+
+	return 0;
+}
